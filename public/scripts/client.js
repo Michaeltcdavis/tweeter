84f78732ab7a4c137a03b78950b631c09c103c55
Michaeltcdavis <michaeltcdavis@gmail.com>
2022-10-13 00:54:30 +0000

    added functions for the client to replace the list of tweets without refreshing the page, and for adding an error message to the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,22 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 $(() => {
+
   const getDaysAgo = function (earlierDate) {
-    console.log(Date.now());
-    const dayNumMinutes = (Date.now() - earlierDate) / (1000 * 60);
-    const dayNumHours = dayNumMinutes / 60;
-    const dayNumDays = dayNumHours / 24;
-    const dayNumYears = dayNumDays / 365;
-    if (dayNumMinutes < 60) {
-      return `${Math.floor(dayNumMinutes)} minute${dayNumMinutes > 1 ? 's' : ''} ago`
-    }
-    if (dayNumMinutes < 1400) {
-      return `${Math.floor(dayNumHours)} hour${dayNumHours > 1 ? 's' : ''} ago`
-    }
-    if (dayNumMinutes < 525600) {
-      return `${Math.floor(dayNumDays)} day${dayNumDays > 1 ? 's' : ''} ago`
-    }
-    return `${Math.floor(dayNumYears)} year${dayNumYears > 1 ? 's' : ''} ago`
+    return timeago.format(earlierDate);
   }
 
   const createTweetElement = function (tweetObject) {
@@ -43,45 +30,40 @@ $(() => {
 
   const renderTweets = function (tweets) {
     for (let tweet of tweets) {
-      $('#tweets-container').append(createTweetElement(tweet));
+      $('#tweets-container').prepend(createTweetElement(tweet));
     }
   }
-  
-  //TESTING FOR RENDERTWEETS FUNCTION-------------------
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  //----------------------------------------------------
-  renderTweets(data);
 
   $('section.new-tweet form').on('submit', function (event) {
     event.preventDefault();
     const form = $(event.target);
-    //tweetContent = form.find('textarea').val();
-    console.log(form.serialize());
+    tweetContent = form.find('textarea').val();
+    form.find("label[for='tweet-text']").text('');
+    if (tweetContent.length > 140) {
+      form.find("label[for='tweet-text']").text('Tweet can only be up to 140 characters');
+      return;
+    } 
+    if (!tweetContent || (tweetContent.length < 1)) {
+      form.find("label[for='tweet-text']").text('Tweet is empty');
+      return;
+    } 
+    console.log(tweetContent)
     $.ajax('/tweets', { data: form.serialize(), method: 'POST' })
+      .then(() => {
+        form.find('textarea').val('');
+        $('#tweets-container').empty();
+        loadTweets();
+      })
       .catch((err) => console.log(err));
   });
+
+  const loadTweets = function () {
+    $.ajax('/tweets', { method: 'GET' })
+      .then((tweets) => {
+        renderTweets(tweets)
+      })
+      .catch((err) => console.log(err));
+  }
+
+  loadTweets();
 })
