5ee86fe98ca2a27335c3ee6a6a54c8179fbc57f7
Michaeltcdavis <michaeltcdavis@gmail.com>
2022-10-20 17:09:07 +0000

    edited sass sheets to add main section width

--- public/scripts/composer-char-directory.js
+++ public/scripts/composer-char-directory.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+
   $('#tweet-text').on('input', function () {
     const remainingChars = 140 - this.value.length;
     if (remainingChars < 0) {
@@ -7,5 +8,8 @@ $(document).ready(() => {
       $(this).siblings('div.below-input').children('output').css('color', '#545149');
     }
     $(this).siblings('div.below-input').children('output').text(remainingChars);
+    // expand text area
+    this.style.height = "";
+    this.style.height = this.scrollHeight*1.05 + "px";
   });
 });
